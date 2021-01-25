6e53e7063a149542e233c9799d2c359141891271
MatiiMartinez <matiase.martinez@aden.org>
2021-01-25 09:24:10 -0300

    Fix

--- src/components/ui/organisms/main-home/Team.js
+++ src/components/ui/organisms/main-home/Team.js
@@ -9,61 +9,55 @@ import { deleteItem } from "../../../../mocks/funciones";
 import { Divider } from "@material-ui/core";
 
 export default function Team() {
-  return (
-    <div>
-      <Header>
-        <Title>Equipo</Title>
-        <New />
-      </Header>
-      <Grid>
-        <HeaderGrid>
-          <span></span>
-          <span>Email</span>
-          <span>Estado</span>
-          <span>Última conexión</span>
-        </HeaderGrid>
-        <Divider />
-        {teamMock.map((item, index) => (
-          <Card item={item} key={index} deleteItem={deleteItem} />
-        ))}
-      </Grid>
-    </div>
-  );
+    return (
+        <div>
+            <Header>
+                <Title>Equipo</Title>
+                <New />
+            </Header>
+            <Grid>
+                <HeaderGrid>
+                    <span></span>
+                    <span>Email</span>
+                    <span>Estado</span>
+                    <span>Última conexión</span>
+                </HeaderGrid>
+                <Divider />
+                {teamMock.map((item, index) => (
+                    <Card item={item} key={index} deleteItem={deleteItem} />
+                ))}
+            </Grid>
+        </div>
+    );
 }
 
 const Header = styled.div`
-  display: flex;
-  justify-content: space-between;
-  align-items: center;
-  margin-bottom: 2rem;
+    display: flex;
+    justify-content: space-between;
+    align-items: center;
+    margin-bottom: 2rem;
 `;
 
 const Title = styled.h1`
-  color: #b31d15;
-  font-size: 2rem;
-  font-weight: 900;
-  line-height: 1rem;
-  margin: 0;
-`;
-
-const SubTitle = styled.p`
-  color: #616161;
-  font-size: 1rem;
-  margin: 1rem 0;
+    color: #b31d15;
+    font-size: 2rem;
+    font-weight: 900;
+    line-height: 1rem;
+    margin: 0;
 `;
 
 const Grid = styled.div`
-  display: flex;
-  flex-direction: column;
-  row-gap: 1rem;
+    display: flex;
+    flex-direction: column;
+    row-gap: 1rem;
 `;
 
 const HeaderGrid = styled.span`
-  display: grid;
-  grid-template-columns: 1fr 6fr 1fr 2fr;
-  padding: 0 1rem;
+    display: grid;
+    grid-template-columns: 1fr 6fr 1fr 2fr;
+    padding: 0 1rem;
 
-  span:nth-child(4) {
-    text-align: end;
-  }
+    span:nth-child(4) {
+        text-align: end;
+    }
 `;
