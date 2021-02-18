2c434bb95e5ba7fef6eedb39326f2792b3a8929b
MatiiMartinez <matiase.martinez@aden.org>
2021-02-18 15:57:25 -0300

    .

--- src/redux/store.js
+++ src/redux/store.js
@@ -5,10 +5,9 @@ import rootSaga from "./sagas";
 
 const sagaMiddleware = createSagaMiddleware();
 
-const store = compose(
-    applyMiddleware(sagaMiddleware),
-    window.devToolsExtension && window.devToolsExtension()
-)(createStore)(rootReducer);
+const store = compose(applyMiddleware(sagaMiddleware))(createStore)(
+  rootReducer
+);
 
 sagaMiddleware.run(rootSaga);
 
